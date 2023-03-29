28a179151964c8af04c83131d7cb888578206b92
Diego Maia <ldgmaia@gmail.com>
2023-03-29 19:26:21 -0400

    Added vitest coverage and UI

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -48,7 +48,7 @@ describe('Register Use Case', (O) => {
       password: '123456',
     })
 
-    expect(() =>
+    await expect(() =>
       registerUseCase.execute({
         name: 'Jon Snow',
         email,
