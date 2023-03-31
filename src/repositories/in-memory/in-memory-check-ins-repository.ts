fb198e9ea12b03ea9cda20d609f02238ff910485
Diego Maia <ldgmaia@gmail.com>
2023-03-30 20:46:46 -0400

    Started adding TDD to use cases

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { CheckIn, Prisma } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { CheckInsRepository } from '../check-ins-repositories'
@@ -16,5 +17,25 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     this.items.push(checkIn)
     return checkIn
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDay = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDay) {
+      return null
+    }
+
+    return checkInOnSameDay
   }
 }
--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,22 +1,92 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { CheckInUseCase } from './check-in'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', (O) => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: '01',
+      title: 'Gym',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+      description: '',
+      phone: '',
+    })
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
     const { checkIn } = await sut.execute({
       userId: '01',
       gymId: '01',
+      userLatitude: 0,
+      userLongitude: 0,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice on the same day', async () => {
+    vi.setSystemTime(new Date(2021, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      userId: '01',
+      gymId: '01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    await expect(
+      sut.execute({
+        userId: '01',
+        gymId: '01',
+        userLatitude: 0,
+        userLongitude: 0,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice on different days', async () => {
+    vi.setSystemTime(new Date(2021, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      userId: '01',
+      gymId: '01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    vi.setSystemTime(new Date(2021, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      userId: '01',
+      gymId: '01',
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+
+    // await expect(
+    //   sut.execute({
+    //     userId: '01',
+    //     gymId: '01',
+    //   }),
+    // ).resolves.toBeTruthy()
   })
 })
